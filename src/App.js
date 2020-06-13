abdc1fea39ae9d239d08b0536c6dc4e612908bc6
Rivaan Ranawat <namanrivaan@gmail.com>
2020-06-13 23:54:53 +0530

    Face Box Introduced

--- src/App.js
+++ src/App.js
@@ -30,7 +30,8 @@ class App extends Component {
     super();
     this.state = {      
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box: {},
     }
   }
 
@@ -38,30 +39,43 @@ class App extends Component {
     this.setState({input : event.target.value});
   }
 
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box: box});
+  }
+
   onButtonSubmit = () => {
     // updates image url with input
     this.setState({imageUrl: this.state.input});
-    console.log("click");
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
-      function(response) {
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-      },
-      function(err) {
-        // there was an error
-      }
-  );
+
+    // getting the picture with clarifai api
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .catch(err => console.log(err))
   }
   render() {
     return (
     <div className="App">
     <Particles className='particles'
               params={particleOptions}
-            />
+    />
     <Navigation/>
     <Logo />
     <Rank/>
     <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
-    <FaceRecognition imageUrl={this.state.imageUrl}/>
+    <FaceRecognition imageUrl={this.state.imageUrl} box={this.state.box}/>
     </div>
   );
   }
